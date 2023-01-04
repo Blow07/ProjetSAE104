506012b881f32ef83379e770de06e70af31badc2
Blow07 <boyemouhamed43@gmail.com>
2023-01-04 12:12:34 +0100

    Initial commit

---
+++ src/Controller/BoyeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BoyeController extends AbstractController
+{
+    #[Route('/boye', name: 'app_boye')]
+    public function index(): Response
+    {
+        return $this->render('boye/index.html.twig', [
+            'controller_name' => 'BoyeController',
+        ]);
+    }
+    #[Route('/theme', name: 'app_boye2')]
+    Public function verstheme(){
+    return $this->render('boye/theme.html.twig');
+    }
+    #[Route('/blog', name: 'app_boye3')]
+    Public function versblog(){
+    return $this->render('boye/blog.twig');
+    }
+}
